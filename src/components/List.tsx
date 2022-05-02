cc589d0122254884cc273c3f1ba90f790cfd2be0
Vinutana <vinutanavm@gmail.com>
2022-05-02 19:20:04 +0530

    session 9

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,86 +1,29 @@
-import React from "react";
-import "./List.css";
-import Item from "./Item";
-import { StoryType } from "../types";
-import { useState } from "react";
+const listOfItems = [
+  {
+    title: "Learn React",
+    url: "https://eprint.iacr.org/2021/1022",
+    createdAt: "2011-12-12",
+    num_comments: 12,
+    objectId: 1,
+  },
+  {
+    title: "Learn TypeScript",
+    url: "https://eprint.iacr.org/2021/1022",
+    createdAt: "2012-12-12",
+    num_comments: 1,
+    objectId: 2,
+  },
+];
 
-type ListProps = {
-  listOfItems: Array<StoryType>;
-};
-
-const SORT_COLUMNS: any = {
-  title: (a: StoryType, b: StoryType) => a?.title?.localeCompare(b?.title),
-  url: (a: StoryType, b: StoryType) => a?.url?.localeCompare(b?.url),
-  author: (a: StoryType, b: StoryType) => a?.author?.localeCompare(b?.author),
-  num_comments: (a: StoryType, b: StoryType) =>
-    a?.num_comments - b?.num_comments,
-};
-
-const CustomSortHeaders = ({ headers, onClick, sortInfo }: any) => {
-  return headers.map(([column, displayName]: any) => (
-    <CustomSortHeader
-      key={column}
-      column={column}
-      displayName={displayName}
-      onClick={onClick}
-      sortInfo={sortInfo}
-    />
-  ));
-};
-
-const CustomSortHeader = ({ column, displayName, onClick, sortInfo }: any) => {
-  return (
-    <th onClick={() => onClick(column)}>
-      {displayName}
-      {"        "}
-      {sortInfo.column === column && (sortInfo.isDesc ? "↓" : "↑")}
-    </th>
-  );
-};
-
-const List = ({ listOfItems }: ListProps) => {
-  const [sortInfo, setSortInfo] = useState(() => {
-    console.log("Initializing sort info state");
-    return { column: "none", isDesc: false };
-  });
-  const newListOfItems = [...listOfItems];
-  newListOfItems.sort(SORT_COLUMNS[sortInfo.column]);
-  if (sortInfo.isDesc) {
-    newListOfItems.reverse();
-  }
-
-  function handleSort(column: "title" | "url" | "author" | "num_comments") {
-    setSortInfo((prev) => {
-      return { column, isDesc: !prev.isDesc };
-    });
-  }
-
+const List = () => {
   return (
     <div>
-      <table>
-        <thead>
-          <tr>
-            <CustomSortHeaders
-              headers={[
-                ["title", "Title"],
-                ["url", "URL"],
-                ["author", "Author"],
-                ["num_comments", "Comments"],
-              ]}
-              onClick={handleSort}
-              sortInfo={sortInfo}
-            />
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {newListOfItems.map((item) => (
-            <Item key={item.objectID} item={item} />
-          ))}
-        </tbody>
-      </table>
+      <ul>
+        <li>{listOfItems[0].title}</li>
+        <li>{listOfItems[0].url}</li>
+      </ul>
     </div>
   );
 };
 
-export default React.memo(List);
+export default List;
--- src/index.tsx
+++ src/index.tsx
@@ -3,25 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+
+const name = "Rahul";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
 root.render(
-  <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register/:uuid" element={<Register />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </BrowserRouter>
-  </>
+  <React.StrictMode>
+    <App firstName={name} age={12} />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
